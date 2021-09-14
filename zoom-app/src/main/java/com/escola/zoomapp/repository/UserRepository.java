ece16a85e62272b074887d45a501e46acccf9562
landson <randelsilva1990@gmail.com>
2021-09-14 19:38:31 -0300

    criando o repository user e poster

---
+++ zoom-app/src/main/java/com/escola/zoomapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.escola.zoomapp.repository;
+
+import com.escola.zoomapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
