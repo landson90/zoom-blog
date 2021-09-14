c0457270582a47a4abfa6f91b1923c927e0018c2
landson <randelsilva1990@gmail.com>
2021-09-14 19:31:43 -0300

    mapeando o relacionamento entre o post e usuario.

--- zoom-app/src/main/java/com/escola/zoomapp/model/Post.java
+++ zoom-app/src/main/java/com/escola/zoomapp/model/Post.java
@@ -1,14 +1,29 @@
 package com.escola.zoomapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table( name = "tb_poster")
 public class Post {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String description;
     private String title;
     private String content;
+
+    @ManyToOne
+    @JoinColumn( name = "user_id")
+    private User user;
 }
