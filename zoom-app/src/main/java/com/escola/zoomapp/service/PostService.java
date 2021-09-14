543b983015923e4a099f4c2901661cd2370d7583
landson <randelsilva1990@gmail.com>
2021-09-14 19:41:08 -0300

    criando os service para user e poster

---
+++ zoom-app/src/main/java/com/escola/zoomapp/service/PostService.java
@@ -0,0 +1,16 @@
+package com.escola.zoomapp.service;
+
+import com.escola.zoomapp.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+}
---
+++ zoom-app/src/main/java/com/escola/zoomapp/service/UserService.java
@@ -0,0 +1,16 @@
+package com.escola.zoomapp.service;
+
+import com.escola.zoomapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
