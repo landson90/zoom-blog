82320b4c4bc6c4d97d86bc43b72d4b3046d98075
landson <randelsilva1990@gmail.com>
2021-09-16 17:09:37 -0300

    criando o metodo no controller para salvar um post

--- zoom-app/src/main/java/com/escola/zoomapp/controller/PostController.java
+++ zoom-app/src/main/java/com/escola/zoomapp/controller/PostController.java
@@ -1,7 +1,11 @@
 package com.escola.zoomapp.controller;
 
+import com.escola.zoomapp.dto.PostDTO;
 import com.escola.zoomapp.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,10 @@ public class PostController {
     @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
+    }
+
+    public ResponseEntity<PostDTO> store(@RequestBody PostDTO postDTO) {
+        PostDTO postAfterCreate = this.postService.postCreate(postDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(postAfterCreate);
     }
 }
--- zoom-app/src/main/java/com/escola/zoomapp/service/PostService.java
+++ zoom-app/src/main/java/com/escola/zoomapp/service/PostService.java
@@ -1,5 +1,6 @@
 package com.escola.zoomapp.service;
 
+import com.escola.zoomapp.dto.PostDTO;
 import com.escola.zoomapp.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,10 @@ public class PostService {
     @Autowired
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    public PostDTO postCreate(PostDTO postDTO) {
+        
+
     }
 }
